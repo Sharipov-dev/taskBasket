97bb14fc33025d7becefff0cb032928bf3c81ced
Arthur <arturka0505@gmail.com>
2024-06-21 07:43:47 -0400

    Good

--- demo/src/main/java/apples/demo/repo/GoodRepository.java
+++ demo/src/main/java/apples/demo/repo/GoodRepository.java
@@ -4,9 +4,12 @@ import apples.demo.entity.Good;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GoodRepository extends JpaRepository<Good, Long> {
     Optional<Good> getGoodByName(String name);
+
 }
--- demo/src/main/java/apples/demo/service/impl/GoodServiceImpl.java
+++ demo/src/main/java/apples/demo/service/impl/GoodServiceImpl.java
@@ -6,33 +6,68 @@ import apples.demo.repo.GoodRepository;
 import apples.demo.service.IGoodService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
-@AllArgsConstructor
 public class GoodServiceImpl implements IGoodService {
 
+    @Autowired
     GoodRepository goodRepository;
+
+    private Map<Long, Object> goodMap = new HashMap<>();
+
+    public GoodServiceImpl(){
+        for(Good good : goodRepository.findAll()){
+            goodMap.put(good.getId(), new Object());
+        }
+    }
+
+
+//    @Override
+//    @Transactional
+//    public synchronized Integer getGoodByName(String name, int amount) {
+//        Good good = goodRepository
+//                .getGoodByName(name)
+//                .orElseThrow(
+//                () -> new RuntimeException(name + " is not found"));
+//
+//        if(good.getAmount() < amount){
+//            throw new NotEnoughGoodsException(name, amount, good.getAmount());
+//        } else {
+//            good.setAmount(good.getAmount() - amount);
+//            Good savedGood = goodRepository.save(good);
+//
+//            Integer totalCost = savedGood.getPrice() * amount;
+//            return totalCost;
+//
+//        }
+//    }
 
     @Override
     @Transactional
-    public synchronized Integer getGoodByName(String name, int amount) {
+    public Integer getGoodByName(String name, int amount) {
         Good good = goodRepository
                 .getGoodByName(name)
                 .orElseThrow(
-                () -> new RuntimeException(name + " is not found"));
+                        () -> new RuntimeException(name + " is not found"));
 
-        if(good.getAmount() < amount){
-            throw new NotEnoughGoodsException(name, amount, good.getAmount());
-        } else {
-            good.setAmount(good.getAmount() - amount);
-            Good savedGood = goodRepository.save(good);
+        synchronized (goodMap.get(good.getId())){
+            if(good.getAmount() < amount){
+                throw new NotEnoughGoodsException(name, amount, good.getAmount());
+            } else {
+                good.setAmount(good.getAmount() - amount);
+                Good savedGood = goodRepository.save(good);
 
-            Integer totalCost = savedGood.getPrice() * amount;
-            return totalCost;
+                Integer totalCost = savedGood.getPrice() * amount;
+                return totalCost;
 
+            }
         }
-
-
     }
+
+
 }
